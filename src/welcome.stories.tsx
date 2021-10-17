820a21b6c77a4c5d34f7fb04061edc119208fbe7
sqlnice <472252510@qq.com>
2021-10-17 21:01:57 +0800

    test travis

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -8,7 +8,7 @@ storiesOf('Welcome', module).add(
       <>
         <h1>欢迎来到 madara 组件库</h1>
         <p>madara 是一个自我使用的组件库，使用 React Hooks 和 typescript</p>
-        <h3>安装试试</h3>
+        <h3>安装试试~</h3>
         <code>npm install madara-ui --save</code>
         <p></p>
       </>
