0b0655e8bdd2e042fb1435d73e3ff1b56b4d47eb
时启龙 <shiqilong@beyondcent.com>
2022-05-06 11:01:10 +0800

    feat：add docs

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React from 'react'
+import { storiesOf } from '@storybook/react'
 
 storiesOf('Welcome', module).add(
   'welcome',
@@ -7,12 +7,12 @@ storiesOf('Welcome', module).add(
     return (
       <>
         <h1>欢迎来到 madara 组件库</h1>
-        <p>madara 是一个自我使用的组件库，使用 React Hooks 和 typescript</p>
+        <p>madara 是一个实验性的小型组件库，使用 React Hooks 和 typescript</p>
         <h3>安装试试~</h3>
-        <code>npm install madara-ui --save</code>
+        <code>npm install madara-ui -S</code>
         <p></p>
       </>
-    );
+    )
   },
   { info: { disable: true } }
-);
+)
